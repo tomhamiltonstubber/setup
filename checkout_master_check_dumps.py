a703d0b814fbf02d58002faca4509d8090576a12
tomhamiltonstubber <tomhamiltonstubber@gmail.com>
2018-08-10 12:01:59 +0100

    Update

--- checkout_master_check_dumps.py
+++ checkout_master_check_dumps.py
@@ -1,13 +1,16 @@
 import os
-from devtools import sformat
-
-dump_files = [fn for fn in os.listdir('.') if '.dump' in fn]
-if dump_files:
-    print()
-    print('\n'.join(dump_files))
-    i = input(sformat('You have backups here. Would you like to delete them? [y]/n ', sformat.bold))
-    if not i or i == 'y':
-        for fn in dump_files:
-            os.system('shred -uz ' + fn)
-            print('Shredded ' + fn)
+try:
+    from devtools import sformat
+except ModuleNotFoundError:
+    pass
+else:
+    dump_files = [fn for fn in os.listdir('.') if '.dump' in fn]
+    if dump_files:
+        print()
+        print('\n'.join(dump_files))
+        i = input(sformat('You have backups here. Would you like to delete them? [y]/n ', sformat.bold))
+        if not i or i == 'y':
+            for fn in dump_files:
+                os.system('shred -uz ' + fn)
+                print('Shredded ' + fn)
 os.system('git checkout master')
--- dl_backup.py
+++ dl_backup.py
@@ -12,14 +12,22 @@ def main():
         print(sformat(f'{id:>10}', sformat.yellow), sformat(date, sformat.green))
     print()
     default_id = lines[0][0]
-    selected_id = input(f'Select the ID [{default_id}]: ') or default_id
+    def get_id():
+        s_id = input(f'Select the ID [{default_id}]: ') or default_id
+        try:
+            assert dict(lines)[s_id]
+            return s_id
+        except KeyError:
+            print(f'Bad key: {s_id}')
+            get_id()
+    selected_id = get_id()
     file_name = f'{selected_id}-{dict(lines)[selected_id]}.dump'
     print('Download backup:', selected_id)
     path = './AATutorCruncher/'
     subprocess.run(f'curl -o {path}{file_name} `heroku pg:backups:url {selected_id}` ', shell=True)
     print('Restoring DB')
     subprocess.run(f'{path}scripts/resetdb.sh')
-    subprocess.run(f'cd .. && pg_restore --verbose --clean --no-acl --no-owner -h localhost -U postgres -j 12 -d tutorcruncher2 {path}{file_name}', shell=True)
+    subprocess.run(f'cd {path} && pg_restore --verbose --clean --no-acl --no-owner -h localhost -U postgres -j 12 -d tutorcruncher2 {file_name}', shell=True)
 
 
 if __name__ == '__main__':
