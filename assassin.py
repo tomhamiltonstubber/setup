b5c73ab6fa1146bc36a3e94ce1a0f40f688ccd35
Tom Hamilton Stubber <tomhamiltonstubber@gmail.com>
2023-03-17 14:41:12 +0000

    Assasin

--- assassin.py
+++ assassin.py
@@ -1,64 +1,79 @@
 import copy
 import random
+import os
 
 weapons = [
     "Another assassin's mobile phone (Not yours)",
-    'Christmas Ornament',
-    'Rolling Pin',
-    'Wifi Password',
-    'Butter',
-    'Teapot',
-    'Oak leaf',
-    'Right wellington boot',
-    'Loo roll',
-    'Tampon',
-    'Phone charger',
-    'Slice of cake or ham',
-    'Thick book',
-    'Eggcup',
-    'Lamp shade',
-    'Pillow',
-    'Framed picture',
-    'Jug of water',
-    'Bottle of whiskey',
-    "Pair of boxers",
-    'Jenga',
-    'Pottery animal',
+    'Some butter (in or out of wrapper)',
+    'A teapot or cafetierre (If one not available, jug of water)',
+    'A loo roll',
+    'A bar of soap',
+    'A phone charger',
+    'A book',
+    'An eggcup',
+    'A pillow',
+    'A jug of water',
+    'A bottle of wine',
+    'A pair of boxers',
+    'A pot/pan',
     'Light bulb',
     'Peppercorn',
-    "Pair of knickers",
-    'Passport',
-    'Mug',
-    "Megan's collar",
-    'Bottle of Burren Balsamics',
-    'Multicoloured sock',
+    'Pair of knickers',
+    'A credit/debit card',
+    'A mug',
+    'A bracelet',
+    'A multicoloured sock',
+    'A pair of spectables/sunglasses',
 ]
 
 rooms = [
-    'Kitchen',
-    'Sitting room',
-    'Hall',
-    'Dorm (with the bunk beds)',
-    'A bedroom (not the dorm)',
+    'the kitchen',
+    'the sitting room',
+    'the hall',
+    'the games room'
+    'any bathroom',
+    'any bedroom',
+    'any outside area',
+    'the kitchen',
+    'the sitting room',
+    'the sitting room',
+    'the games room',
+    'any outside',
+    'any outside',
 ]
 
 killers = [
     'Iona',
-    'Luc',
-    'Ben',
-    'Rand',
-    'Kirsty',
-    'Ollie',
-    'Zoe',
+    'Tom',
+    'Chloe',
+    'Archie',
+    'Isobel',
+    'Jack',
+    'Hector',
+    'George',
+    'Chiara',
+    'Olivia',
+    'Bruno',
+    'Harry',
 ]
 
 
-random.shuffle(killers)
-random.shuffle(weapons)
+def run():
+    random.shuffle(killers)
+    random.shuffle(weapons)
+    random.shuffle(rooms)
+    victims = [killers[-1]] + killers[:-1]
+    k_v_w_r = {k: {'v': v, 'w': w, 'r': r} for k, v, w, r in zip(killers, victims, weapons[:len(killers)], rooms[:len(killers)])}
 
-victims = [killers[-1]] + killers[:-1]
+    while True:
+        player = input('Enter your full first name, killer: ').title()
+        if v_w_r := k_v_w_r.get(player):
+            print(f'You are killing {v_w_r["v"]} with {v_w_r["w"]} in {v_w_r["r"]}. Write this down and do not tell anyone!')
+            input('Press any key to clear the results.')
+            os.system('clear')
+        else:
+            print('This name is not in the list! Remember to use your full first name, no nicknames')
 
-k_v_w = zip(killers, victims, weapons[:len(killers)])
 
-for x, y, z in k_v_w:
-    print(f'You are killing {y} with a {z}\n in {}')
+if __name__ == '__main__':
+    run()
